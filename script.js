e7f95ae0e661f4f991b2072da6b51c30ee26bc85
BHAVANI28122 <108610488+BHAVANI28122@users.noreply.github.com>
2023-03-17 19:08:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function palindrome(str){
 	let str1 = str.replaceAll(" ","");
 	const string = str1.reverse();
 	if( str1 == string){
-		return false;
+		return true;
 	}	
-  return true;
+  return false;
 }
 module.exports = palindrome
