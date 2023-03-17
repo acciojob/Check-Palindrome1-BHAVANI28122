972b7f6d012b3867e77904011dbc173db3fd4ed6
BHAVANI28122 <108610488+BHAVANI28122@users.noreply.github.com>
2023-03-17 19:13:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 function palindrome(str){
 
 	let str1 = str.replaceAll(" ","");
-	for(let i=0;i<str1.length/2;i++){
+	for(let i=0;i<str1.length/2-1;i++){
 		if(str1.charAt[i] != str1.charAt[str1.length - 1 - i]){
 			return false;
 		}
