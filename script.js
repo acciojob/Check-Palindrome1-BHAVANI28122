53a4a375b41ebaa04d93280d40ea9c8e037df657
BHAVANI28122 <108610488+BHAVANI28122@users.noreply.github.com>
2023-03-17 19:31:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 // complete the given function
 
+   function reverse( str )
+    {
+     
+      let rev_str = "";
+      for( let i = str.length-1 ;i >= 0 ;i--)
+      {
+        rev_str+= str[i];
+      }
+
+      return rev_str;
+    }
 function palindrome(str){
+let  reverse_str = reverse(str);
+     
+      if( reverse_str === str)
+      {
+       return true;
+      }
 
-	let string = "";
-	for(let i=str.length-1;i>=0;i--){
-		string = string +str.charAt[i];
-	}
+	return false;
 
-	if(str === string){
-		return true;
-	}
-   return false;
 }
 module.exports = palindrome
